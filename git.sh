3ae787d5340c7546acb2a90dac878c574b1189f6
root <tchattua@gmail.com>
2022-08-20 00:49:18 +0000

    021920222038 update

--- git.sh
+++ git.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-echo "git status"
 sudo git status
-sleep 4
 
-echo "git add ."
 sudo git add .
-sleep 4
 
-echo "git commit"
 sudo git commit -m "021920222038 update"
-sleep 4
 
-echo "git push origin main"
 sudo git push origin main
